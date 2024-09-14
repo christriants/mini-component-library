169966f7f0dfe260cf7f935edb9d808d52178982
christriants <christopher.triantafilis@livestream.com>
2024-09-14 12:24:36 -0400

    Build Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ChevronDown } from 'react-feather';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
@@ -9,10 +10,58 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper tabIndex={0}>
+      <Value>{displayedValue}</Value>
+    <StyledSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </StyledSelect>
+    <ChevronDownIcon id="chevron-down" size={12} />
+
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 8px;
+  width: fit-content;
+  padding: 8px 12px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  line-height: 1.2;
+  align-items: center;
+  gap: 8px;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus {
+    border: 1px solid blue;
+  }
+`;
+
+const Value = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+`;
+
+const StyledSelect = styled.select`
+  border: none;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const ChevronDownIcon = styled(Icon)`
+  font-weight: 700;
+`;
 
 export default Select;

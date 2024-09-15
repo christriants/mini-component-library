ceae54e88f074be489fb1a94a72a2bd6b14c0f47
christriants <christopher.triantafilis@livestream.com>
2024-09-14 22:16:13 -0400

    Build IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+
+const SIZES = {
+  small: {
+    '--width': '150px',
+    '--fontSize': '14px',
+    '--borderBottom': '1px solid black',
+  },
+  large: {
+    '--width': '300px',
+    '--fontSize': '18px',
+    '--borderBottom': '2px solid black',
+  },
+}
 
 const IconInput = ({
   label,
@@ -13,7 +26,57 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const inputRef = useRef();
+  const styles = SIZES[size];
+  return (
+    <InputWrapper style={styles}>
+        <Icon 
+          id={icon} 
+          size={size === 'sm' ? 8 : 12} 
+          onClick={() => inputRef?.current.focus()}
+        />
+        <Input 
+          aria-label={label}
+          ref={inputRef} 
+          placeholder={placeholder}
+          style={{
+            '--width': width + 'px',
+          }}
+        />
+    </InputWrapper>
+  )
 };
+
+const InputWrapper = styled.div`
+  width: var(--width);
+  display: flex;
+  flex-direction: row;
+  color: ${COLORS.gray700};
+  font-size: var(--fontSize);
+  align-items: center;
+  gap: 4px;
+  border-bottom: var(--borderBottom); 
+  font-weight: 400;
+
+  &:focus-within {
+    outline: 1px solid blue;
+  }
+
+  &:hover {
+    color: black;
+  }
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none; 
+  color: ${COLORS.gray700};
+  width: var(--width);
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
 
 export default IconInput;
